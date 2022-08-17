7d7797f94018f85ee10a1936e86596892537d77a
Fedor <farlep1970@yandex.ru>
2022-08-17 23:04:31 +0300

    task_23

---
+++ task_23/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Таблица кубов чисел от 1 до N");
+Console.Write("Введите число N: ");
+int n = Math.Abs(int.Parse(Console.ReadLine()!));
+int index = 1;
+while (index <= n)
+{
+    int pow = (int) Math.Pow(index, 3);
+    Console.Write(pow + " ");
+    index++;
+}
+Console.WriteLine();
